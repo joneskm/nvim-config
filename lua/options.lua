9d4dd803421c0a098481c260dfe5d21d61e113b7
Kevin Jones <kevin.jonesat@gmail.com>
2025-07-17 15:52:50 +0100

    Update CursorHold updatetime
    The keyboard repeat delay is the time between when you press and hold a
    key and when it starts repeating. If the CursorHold updatetime is less
    than this then the CursorHold will trigger before the key starts
    repeating. This will cause the git line info to run, causing a stutter.
    
    Typically the keyboard delay is around 500ms so we set the CursorHold
    updatetime to 600ms.

--- lua/options.lua
+++ lua/options.lua
@@ -2,5 +2,13 @@ require "nvchad.options"
 
 -- add yours here!
 
--- local o = vim.o
+local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
+-- Set how long to wait before CursorHold triggers (in milliseconds)
+-- Default is 4000ms (4 seconds), lower values make it more responsive
+o.updatetime = 600 -- 600ms = responsive but avoids keyboard repeat issues (see below)
+
+-- The keyboard repeat delay is the time between when you press and hold
+-- a key and when it starts repeating. If the updatetime is less than this
+-- then the CursorHold will trigger before the key starts repeating.
+-- This will cause the git line info to return, causing a stutter.
